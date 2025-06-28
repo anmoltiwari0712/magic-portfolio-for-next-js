8d3ea6bbfa7da586a58c771eb68406d86aacfa49
anmol <oanmoltiwari7@gmail.com>
2025-06-28 12:25:16 +0530

    Modified page.tsx to TeamPage.tsx

--- src/app/about/team/page.tsx
+++ src/app/about/team/page.tsx
@@ -1,12 +1,5 @@
-// app/about/team/page.tsx
+import TeamPage from "@/components/about/team/TeamPage";
 
-export default function TeamPage() {
-  return (
-    <main className="max-w-3xl mx-auto py-12 px-4">
-      <h1 className="text-4xl font-bold mb-4">Meet the Team</h1>
-      <p>
-        You're on the team page. This is where we introduce Anmol and Aditya.
-      </p>
-    </main>
-  );
+export default function TeamRoute() {
+  return <TeamPage />;
 }
