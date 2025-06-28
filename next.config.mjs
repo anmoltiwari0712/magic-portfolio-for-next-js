9158401dd77688519cd326c2d358c4a7055110b5
anmol <oanmoltiwari7@gmail.com>
2025-06-28 12:44:39 +0530

    still trying2

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,10 @@ const nextConfig = {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default withMDX(nextConfig);
