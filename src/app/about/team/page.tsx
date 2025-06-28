6117de120cbbf8773a105a889cbf070c121a1d33
anmol <oanmoltiwari7@gmail.com>
2025-06-28 12:36:55 +0530

    still trying

--- src/app/about/team/page.tsx
+++ src/app/about/team/page.tsx
@@ -1,4 +1,5 @@
-import TeamPage from "@/components/about/team/TeamPage";
+import TeamPage from "../../../components/about/team/TeamPage";
+
 
 export default function TeamRoute() {
   return <TeamPage />;
